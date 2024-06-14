25f6bad3df77366fea090e6424e668d9f2cfb300
Thomas Prévost <tprevost@smtp.i3s.unice.fr>
2024-06-14 11:46:54 +0200

    Update sbox construction

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,46 @@ const SBOX_SIZE: usize = RING_SIZE * 2;
 const FUNCTION_INPUT_SIZE: usize = 5;
 const GOOD_BIJECTIVE_RULE_NUMBER: u32 = 1438886595;
 
+// Dire 7 tours: https://iacr.org/archive/crypto2003/27290510/27290510.pdf
+// Melange affine: essentiel pr eliminer le cas 1023 0b1111111111
+// https://github.com/PoustouFlan/SUnbox
+
 fn main() {
-    let mut output_set = HashSet::new();
-    let mut count = 0;
-    for i in 0..(1 << SBOX_SIZE) {
-        let input_bits: [bool; SBOX_SIZE] = unsigned_to_bool_array::<SBOX_SIZE>(i);
-        let round_1 = pair_wise_permutation(input_bits, 5, 3);
-        let round_2 = do_lr_round(round_1, GOOD_BIJECTIVE_RULE_NUMBER);
-        let round_3 = do_lr_round(round_2, GOOD_BIJECTIVE_RULE_NUMBER);
-        let round_4 = do_lr_round(round_3, GOOD_BIJECTIVE_RULE_NUMBER);
-        let round_5 = do_lr_round(round_4, GOOD_BIJECTIVE_RULE_NUMBER);
-        let round_6 = pair_wise_permutation(round_5, 7, 11);
-        let round_7 = do_lr_round(round_6, GOOD_BIJECTIVE_RULE_NUMBER);
-        let output_unsigned = bool_array_to_unsigned::<SBOX_SIZE>(round_7);
-        if round_4[0] == input_bits[0] ^ input_bits[5] ^input_bits[6] ^ input_bits[7] ^ input_bits[8] {
-            count += 1;
-        }
-        println!("{}, ", output_unsigned);
-        output_set.insert(output_unsigned);
+    //let mut output_set = HashSet::new();
+    for i in /*0..(1 << (SBOX_SIZE - 3))*/0..(1 << SBOX_SIZE) {
+
+        /*println!("\\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x} & \\textbf{{0x{:03x}}} & 0x{:03x}\\\\",
+                 i, calc_sbox_value(i),
+                 i+128, calc_sbox_value(i+128),
+                 i+256, calc_sbox_value(i+256),
+                 i+384, calc_sbox_value(i+384),
+                 i+512, calc_sbox_value(i+512),
+                 i+640, calc_sbox_value(i+640),
+                 i+768, calc_sbox_value(i+768),
+                 i+896, calc_sbox_value(i+896),
+        );*/
+
+        let output_unsigned = calc_sbox_value(i);
+        println!("{},", output_unsigned);
+        //output_set.insert(output_unsigned);
     }
-    //println!("Count: {}", count);
     //println!("Output set size: {}", output_set.len());
+}
+
+fn calc_sbox_value(input: usize) -> usize {
+    let input_bits: [bool; SBOX_SIZE] = unsigned_to_bool_array::<SBOX_SIZE>(input);
+    let round_1 = pair_wise_permutation(input_bits, 5, 3);
+    let round_2 = do_lr_round(round_1, GOOD_BIJECTIVE_RULE_NUMBER);
+    let round_3 = do_lr_round(round_2, GOOD_BIJECTIVE_RULE_NUMBER);
+    let round_4 = do_lr_round(round_3, GOOD_BIJECTIVE_RULE_NUMBER);
+    let round_5 = do_lr_round(round_4, GOOD_BIJECTIVE_RULE_NUMBER);
+    let round_6 = pair_wise_permutation(round_5, 7, 11);
+    let round_7 = do_lr_round(round_6, GOOD_BIJECTIVE_RULE_NUMBER);
+    let round_8 = do_lr_round(round_7, GOOD_BIJECTIVE_RULE_NUMBER); // ?
+    let round_9 = do_lr_round(round_8, GOOD_BIJECTIVE_RULE_NUMBER); // ?
+    let round_10 = pair_wise_permutation(round_9, 13, 17);
+    let round_11 = do_lr_round(round_10, GOOD_BIJECTIVE_RULE_NUMBER);
+    bool_array_to_unsigned::<SBOX_SIZE>(round_11)
 }
 
 fn do_lr_round(input_bits: [bool; SBOX_SIZE], rule_number: u32) -> [bool; SBOX_SIZE] {
